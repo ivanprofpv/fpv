883981e3002ebf78fedf180d29965c29ce809fbf
ivanprofpv <akvabanka@gmail.com>
2023-02-06 15:54:24 +0400

    add spec for active_storage

--- spec/models/drone_spec.rb
+++ spec/models/drone_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Drone, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
+
+  it 'Authenticated user have many attached foto to dron-card' do
+    expect(Drone.new.fotos).to be_an_instance_of(ActiveStorage::Attached::Many)
+  end
 end
