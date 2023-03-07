d328713d31a16c8d6e59bcb5bbe30e367dc49186
ivanprofpv <akvabanka@gmail.com>
2023-03-07 20:08:19 +0400

    add form create component, ajax + bootstrap

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,11 +1,30 @@
 class ComponentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_drone, only: %i[ create ]
+
+  def new
+    @component = Component.new
+  end
+
+  def show
+    @components = Component.all
+  end
+
+  def create
+    @component = @drone.components.new(component_params)
+
+    @component.save
+  end
 
   def destroy
     @component.destroy
   end
 
   private
+
+  def component_params
+    params.require(:component).permit(:title, :url, :price, :drone_id, :component_category_id)
+  end
 
   def find_drone
     @drone = Drone.find(params[:drone_id])
