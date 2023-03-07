93278d026f22c35e936fbb16e0a54e3b1b43f47c
ivanprofpv <akvabanka@gmail.com>
2023-03-08 01:05:05 +0400

    add table for components for drone-card

--- app/models/component.rb
+++ app/models/component.rb
@@ -6,6 +6,7 @@ class Component < ApplicationRecord
   accepts_nested_attributes_for :component_category, reject_if: :all_blank, allow_destroy: true
 
   validates :title, presence: true
-  # validates_format_of :url, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
+  validates_format_of :url, allow_blank: true, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
+  # validates :price, numericality: { only_integer: true }
 
 end
