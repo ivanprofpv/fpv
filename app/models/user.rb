f52bac9f4180f32f3794defc5d5807f629493c3a
ivanprofpv <akvabanka@gmail.com>
2023-03-17 15:37:47 +0400

    added authorization and registration by login, to display the login in the profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,35 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :trackable, :confirmable
 
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validate :validate_username
+
   acts_as_voter
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 
   def author?(subject)
     self.id == subject.user_id
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
   end
 end
