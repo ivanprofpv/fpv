55ca93cd09d1efccb6fad0738a2ac42d1063e3dd
ivanprofpv <akvabanka@gmail.com>
2023-03-20 02:35:45 +0400

    add avatar user(profile)

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
+  has_one_attached :avatar, dependent: :destroy
+
   validates_length_of :name, maximum: 50, message: "Shorten the name to 50 characters", allow_blank: true
   validates_length_of :about, maximum: 300, message: "Shorten the about to 300 characters", allow_blank: true
   validates_length_of :city, maximum: 50, message: "Shorten the city to 50 characters", allow_blank: true
