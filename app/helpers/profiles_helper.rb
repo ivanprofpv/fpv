63fae9b3d2ba372d8e0f17a7b9b4a51bc71ea009
ivanprofpv <akvabanka@gmail.com>
2023-03-20 10:36:09 +0400

    added counting drones and user comments, added checking avatars

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,9 @@
 module ProfilesHelper
+  def count_drone_user
+    @drone_user = Drone.where(user_id: current_user).count
+  end
+
+  def count_comment_user
+    @comment_user = Comment.where(user_id: current_user).count
+  end
 end
