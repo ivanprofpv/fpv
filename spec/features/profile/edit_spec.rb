9fec784a7d2a6fba2ba02e3e0a4f564f7a982574
ivanprofpv <akvabanka@gmail.com>
2023-03-23 22:53:46 +0400

    rebuild spec profiles after rebuild routing

--- spec/features/profile/edit_spec.rb
+++ spec/features/profile/edit_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 feature 'User can edit self profile' do
   given(:user) { create(:user) }
+  given(:user_admin) { create(:user, admin: 'true') }
   given(:other_user) { create(:user) }
   given(:profile) { create(:profile, user_id: user.id) }
 
   describe 'Authenticated user', js: true do
     background do
       sign_in(user)
-      visit profile_path
+      visit profile_path(profile)
     end
 
     scenario 'edit profile if author' do
@@ -21,7 +22,7 @@ feature 'User can edit self profile' do
       expect(page).to have_content('new name profile')
     end
 
-    scenario 'do not save (errors)' do
+    scenario 'save profile nil name' do
       click_on 'Edit Profile'
 
       fill_in(id: 'floatingTextareaName', with: '')
@@ -32,9 +33,37 @@ feature 'User can edit self profile' do
     end
   end
 
+  describe 'Authenticated admin user', js: true do
+    background do
+      sign_in(user_admin)
+      visit profile_path(profile)
+    end
+
+    scenario 'edit profile if author' do
+      click_on 'Edit Profile'
+
+      fill_in 'Name', with: 'new name2 profile'
+
+      click_on 'Update'
+
+      expect(page).to have_content('new name2 profile')
+    end
+  end
+
+  describe 'Authenticated other user', js: true do
+    background do
+      sign_in(other_user)
+      visit profile_path(profile)
+    end
+
+    scenario 'do not edit profile if not author' do
+      expect(page).to_not have_content('Edit Profile')
+    end
+  end
+
   describe 'Unauthenticated user', js: true do
     background do
-      visit profile_path
+      visit profile_path(profile)
     end
 
     scenario 'can not see edit button' do
--- spec/features/profile/views_users_profiles_spec.rb
+++ spec/features/profile/views_users_profiles_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 feature 'User can sees your profile' do
   given(:user) { create(:user) }
+  given(:user_admin) { create(:user, admin: 'true') }
   given(:other_user) { create(:user) }
   given(:profile) { create(:profile, user_id: user.id) }
   given(:other_profile) { create(:profile, user_id: other_user.id) }
@@ -19,6 +20,32 @@ feature 'User can sees your profile' do
     end
   end
 
+  describe 'Authenticated other user', js: true do
+    background do
+      sign_in(other_user)
+      visit profile_path(profile)
+    end
+
+    scenario 'sees your profile if author' do
+      expect(page).to have_content("name")
+      expect(page).to have_content user.username
+      expect(page).to_not have_content('Edit Profile')
+    end
+  end
+
+  describe 'Authenticated admin user', js: true do
+    background do
+      sign_in(user_admin)
+      visit profile_path(profile)
+    end
+
+    scenario 'sees profile if admin' do
+      expect(page).to have_content("name")
+      expect(page).to have_content user.username
+      expect(page).to have_content('Edit Profile')
+    end
+  end
+
   describe 'Unauthenticated user', js: true do
     background do
       sign_in(user)
@@ -27,7 +54,7 @@ feature 'User can sees your profile' do
 
     scenario 'can not see edit button' do
       expect(page).to have_content("name")
-      expect(page).to have_content user.username
+      expect(page).to have_content other_user.username
       expect(page).to_not have_content('Edit Profile')
     end
   end
