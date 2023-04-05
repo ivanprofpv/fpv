6504e32d7b81c37d3f534b127061878665434f17
ivanprofpv <akvabanka@gmail.com>
2023-04-06 01:51:34 +0400

    add spec, and cron

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe SearchController, type: :controller do
+  let(:user) { create(:user) }
+  let(:category) { create(:category) }
+  let!(:drone) { create(:drone, title: 'test', user:, category:) }
 
+  describe 'GET #index' do
+      before do
+        get :index, params: { query: 'test' }
+      end
+
+      it 'assigns @search_result' do
+        expect(assigns(:search_result)).to eq([drone])
+      end
+
+      it 'renders index' do
+        expect(response).to render_template :index
+      end
+    end
 end
