8e8f9701f66f8d8ff0534bbfca6d4079499a947a
ivanprofpv <akvabanka@gmail.com>
2023-04-21 10:20:47 +0400

    fix policies for categories and add feature spec

---
+++ spec/features/categories/views_categories_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+feature 'User can sees your profile' do
+  given(:user) { create(:user) }
+  given(:user_admin) { create(:user, admin: 'true') }
+  given(:category) { create(:category) }
+
+  describe 'Authenticated user', js: true do
+    background do
+      sign_in(user)
+      visit categories_path
+    end
+
+    scenario 'sees category' do
+      expect(page).to have_content("DroneCategory")
+    end
+  end
+
+  describe 'Authenticated admin user', js: true do
+    background do
+      sign_in(other_user)
+      visit categories_path
+    end
+
+    scenario 'sees category' do
+      expect(page).to have_content("DroneCategory")
+    end
+  end
+
+  describe 'Unauthenticated user', js: true do
+    background do
+      visit categories_path
+    end
+
+    scenario 'sees category' do
+      expect(page).to have_content("DroneCategory")
+    end
+  end
+end
