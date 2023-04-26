b0691204ffbb917493907665a1ced7e86ff4c23f
ivanprofpv <akvabanka@gmail.com>
2023-04-26 14:45:55 +0400

    add show/hide password in registration page

--- app/javascript/password/pass.js
+++ app/javascript/password/pass.js
@@ -1,9 +1,24 @@
 $(document).on('turbolinks:load', function(){
-  $(".eye_icon").on('click', function() {
-    if ($(".password").attr('type') === 'password') {
-        $(".password").attr('type', 'text');
+  $("#password_icon").on('click', function() {
+    if ($("#password").attr('type') === 'password') {
+        $("#password").attr('type', 'text');
+        $("#password_icon").removeClass('fa-eye');
+        $("#password_icon").addClass('fa-eye-slash');
     } else {
-        $(".password").attr('type', 'password');
+        $("#password_icon").removeClass('fa-eye-slash');
+        $("#password_icon").addClass('fa-eye');
+        $("#password").attr('type', 'password');
+    }
+  });
+  $("#confirm_password_icon").on('click', function() {
+    if ($("#confirm_password").attr('type') === 'password') {
+        $("#confirm_password").attr('type', 'text');
+        $("#confirm_password_icon").removeClass('fa-eye');
+        $("#confirm_password_icon").addClass('fa-eye-slash');
+    } else {
+        $("#confirm_password_icon").removeClass('fa-eye-slash');
+        $("#confirm_password_icon").addClass('fa-eye');
+        $("#confirm_password").attr('type', 'password');
     }
   });
 });
