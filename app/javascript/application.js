3cf09d198e61a19230ffefe153db85cf63451b3a
ivanprofpv <akvabanka@gmail.com>
2023-08-17 10:25:47 +0400

    webpacker destroy

---
+++ app/javascript/application.js
@@ -0,0 +1,19 @@
+// Entry point for the build script in your package.json
+import "./controllers"
+require("trix")
+require("@rails/actiontext")
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("jquery")
+require("comments/edit")
+require("components/add_component")
+require("components/edit")
+require("gallery/dimbox")
+require("password/pass")
+
+require("@popperjs/core")
+
+import "bootstrap"
+import "trix"
+import "@rails/actiontext"
